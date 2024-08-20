52f9fb494f1ccb0d12d49abc9e037bf96e77dfcd
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-08-20 07:28:03 +0100

    updates

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -10,7 +10,7 @@ interface Data {
 const data:Data ={
     brand:"QHS",
     city:"Queens",
-    state:"FL",
+    state:"NY",
     domain: "queensherniaspecialists.com",
     logo: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1719071019/3.3Asset_86_dlilyw.png',
     src:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193730.23282029494!2d-74.03600268163837!3d40.651161344417844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24369470a592b%3A0x4109d18b6c5c7b05!2sQueens%2C%20NY%2C%20USA!5e0!3m2!1sen!2sng!4v1719070961217!5m2!1sen!2sng"
